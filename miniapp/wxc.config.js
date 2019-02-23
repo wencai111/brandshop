d047595a2e9fdc252ad3fa9e4cec9cb5130df86d
autoyu <yucihai@live.com>
2019-02-23 16:29:38 +0800

    请求地址修改成 开发环境

--- miniapp/wxc.config.js
+++ miniapp/wxc.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    apiUrl: "https://shop.wuxinyoudian.com",
-    payUrl: "https://shop.wuxinyoudian.com",
+    apiUrl: "https://shop.wuxincheng.com",
+    payUrl: "https://shop.wuxincheng.com",
     mediaUrl: "https://cdn.osann-china.com"
 };
