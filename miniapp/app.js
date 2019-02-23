15c270c0a9742a871e8f88b383b421c5f558f2e1
autoyu <yucihai@live.com>
2019-02-23 16:06:10 +0800

    增加全局第三方小程序配置

--- miniapp/app.js
+++ miniapp/app.js
@@ -1,64 +1,64 @@
 var config = require("wxc.config.js");
-debugger;
 
 App({
-    onLaunch: function() {
+    onLaunch: function () {
         var _this = this;
+        _this.extConfig = wx.getExtConfigSync();
         wx.checkSession({
-            success: function() {
+            success: function () {
                 var sessionid = wx.getStorageSync("sessionid");
                 var userid = wx.getStorageSync("userid");
                 var userInfo = wx.getStorageSync("userInfo");
-                if(sessionid!=""&&userid!=""&&userInfo){
-                    _this.globalData.openid=sessionid;
-                    _this.globalData.userid=userid;
+                if (sessionid != "" && userid != "" && userInfo) {
+                    _this.globalData.openid = sessionid;
+                    _this.globalData.userid = userid;
                     console.log("storage sessionid = ", sessionid, " and userid = ", userid);
-                    _this.globalData.userInfo=userInfo;
+                    _this.globalData.userInfo = userInfo;
                     console.log("storage userInfo : ", userInfo);
-                }
-                else{
+                } else {
                     console.log("缓存信息时效，重新登录获取用户信息！");
                     _this.wxloginwithsession();
                 }
             },
-            fail: function() {
+            fail: function () {
                 console.log("登录态已经过期，重新登录！");
                 _this.wxloginwithsession();
             }
         });
     },
-    wxloginwithsession: function() {
+    wxloginwithsession: function () {
         var _this = this;
+        var appid = (wx.getExtConfigSync()).appid;
         wx.login({
-            success: function(res) {
-                if(res.code){
-                    console.log("code is "+res.code);
+            success: function (res) {
+                if (res.code) {
+                    console.log("code is " + res.code);
                     wx.request({
                         url: config.apiUrl + "/code2session.aspx",
                         method: "POST",
                         data: {
-                            code: res.code
+                            "jscode": res.code,
+                            "appid": _this.extConfig.appid
                         },
-                        success: function(o) {
+                        success: function (o) {
                             debugger;
                             console.log("remote sessionid : ", o.data);
                             _this.globalData.openid = o.data;
-                             wx.setStorage({
+                            wx.setStorage({
                                 key: "sessionid",
                                 data: o.data
                             });
                         },
-                        fail:function(res){
+                        fail: function (res) {
                             debugger;
                         },
-                        complete:function(res){
+                        complete: function (res) {
                             debugger;
 
                         }
                     });
-                }
-                else{
-                    console.log("登录失败："+res.errMsg);
+                } else {
+                    console.log("登录失败：" + res.errMsg);
                 }
             }
         });
